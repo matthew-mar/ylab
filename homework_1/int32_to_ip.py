f4cf04130c2f32f3804ebcf4d0f3e795535f746d
matthew-mar <mmary.prog@gmail.com>
2022-06-24 22:51:44 +0300

    task 2 complete

---
+++ homework_1/int32_to_ip.py
@@ -0,0 +1,12 @@
+def int32_to_ip(int32):
+    ip_adress = []
+    for i in range(4):
+        ip_adress.append(str(int32 % 256))
+        int32 //= 256
+    return ".".join(ip_adress[::-1])
+
+
+if __name__ == "__main__":
+    assert int32_to_ip(2154959208) == "128.114.17.104"
+    assert int32_to_ip(0) == "0.0.0.0"
+    assert int32_to_ip(2149583361) == "128.32.10.1"
